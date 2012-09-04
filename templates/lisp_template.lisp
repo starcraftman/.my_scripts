8985840c99e5104e702d62cb6435744a164381a8
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2012-09-04 12:16:53 -0400

    Updated lisp template to be clearer, added site.

--- templates/lisp_template.lisp
+++ templates/lisp_template.lisp
@@ -1,8 +1,6 @@
 #|
 (defun func () ; Function definition.
 	(print "test"))
-
-(print `(+ 3 2 4 5)) ; ' Disables the evaluation of list, print it.
 ; Combine boolean with "and" "or" "not" functions. True/False -> t/nil.
 |#
 ;;;; Comment header for a section, use 3 for paragraph, 2 for comment next line, 1 for current line.
@@ -11,12 +9,14 @@
 
 
 #|
-Function Index:
-Anon Functions:
+NOTES!
+Good website -> http://www.cs.cmu.edu/Groups/AI/html/cltl/clm/index.html
+
+ANON FUNCTIONS:
 ((lambda (args) (body)) arg_to_apply_lambda_to): Make an ananoymous function and apply to enclosed args.
 
 ; For global variables enclose with *, i.e. *PI*.
-Variables: 
+VARIABLES: 
 (let ((x 5) (y 2)) (+ x y)): Define a variable.
 (let* ((x 10) (y (* 2 x))) (* x y)): Didn't require nesting y inside x scope because of let*.
 (defparameter name value): Defines a variable that can be modified.
@@ -28,19 +28,19 @@ Variables:
 (eql var1 var2): Check if the two pointers are equal.
 
 ': Escapes everything that follows under its escape.
-Output:
+OUTPUT:
 (print object): Prints its arg.
 (write object): Same as print, doesn't print a new line before current.
 
+LIST MANIPULATION:
 list: Construct a list of the arguments as is.
 append: Merge the contents of the lists into one.
 cons: Append the first arg to the second arg (that is a list).
-
 car: Return head of a list.
 cdr: Return the tail of a list.
 ; Tip: Can use mutliple invocations with caddr -> will take the tail twice then return head.
 
-Predicates:
+PREDICATES:
 null: Return true if arg is null.
 atomp: Return true if arg is atom.
 listp: Return true if argument is a list.
@@ -53,7 +53,7 @@ minusp: Return true if arg is negative.
 evenp: Return true if arg is even.
 oddp: Return true if arg is odd.
 
-Math:
+MATH:
 (sqrt a): Square root of arg.
 (expt a b): Return result of a^b.
 (log a): Return the natural log of a.
@@ -61,12 +61,12 @@ Math:
 (incf x [step]): Increment x by step, if not provided step = 1.
 (decf x [step]): Same as incf, but decrement.
 
-High Order:
+FUNCTION MAPPING:
 (mapcar function list1 list2 list3): Apply function to each list.
 (funcall function arg1 arg2 arg3...): Apply function to each arg, doesn't need to be list.
 (apply function arg1 arg2 ... list): Like funcall, last arg is list.
 
-Control:
+CONTROL:
 (if (then) (else)): Standard control.
 (loop): Create an inifinite loop.
 (dotimes (n 3 t) (body): Do a loop from [0,3). n is var.
@@ -75,7 +75,7 @@ Control:
 (return-from label value): Cause flow to return from labeled block with value.
 (go label): Goto equivalent, use with tagbody.
 
-Blocks:
+BLOCKS:
 (progn body): Progn forces a block and executes each arg in order.
 (block label body): Same as progn but return with return-from label value function call.
 (tagbody body): Make tags with chars outside list, use (go label) to jump.
