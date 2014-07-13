c4a8b76be0e4252d8b6f13617e6cfc2359522530
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-07-12 22:47:27 -0400

    Storing lib builder for future cpp use.

---
+++ archive/GetLibs.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+# Common tools:
+#	pip/pip3 : install packages
+#	pylint/pychecker : verify syntax
+#	unittest package for xUnit.
+""" This module helps setup a fresh install the way I like it. """
+
+# Imports
+from __future__ import print_function
+import argparse
+import glob
+import os
+import shutil
+import subprocess
+import sys
+import urllib
+import zipfile
+
+# Classes
+
+class Progress(object):
+    """ Draw a simple progress bar. """
+    def __init__(self, tick, empty, total_ticks):
+        self.tick = tick
+        self.empty = empty
+        self.total_ticks = total_ticks
+        self.num_ticks = 0
+        self.old_percent = 0
+    def check_percent(self, new_percent):
+        """ Helper, check if passed a point. """
+        if new_percent >= self.old_percent + self.tick_threshold():
+            self.old_percent = new_percent
+            return True
+        else:
+            return False
+    def draw(self):
+        """ Draw a bar for progress after incrementing. """
+        self.num_ticks += 1
+        num_empty = self.total_ticks - self.num_ticks
+        line = "Download Progress: [%s%s]\n" % (self.tick * self.num_ticks,
+            self.empty * num_empty)
+        sys.stdout.write(line)
+        sys.stdout.flush()
+    def tick_threshold(self):
+        """ Return number of % per tick on bar. """
+        return 100 / self.total_ticks
+    @staticmethod
+    def default_prog():
+        """ Factory method, makes a bar with defaults. """
+        return Progress('*', '-', 20)
+
+class PDir(object):
+    """ Pushd analog for personal use. """
+    dirs = []
+    @staticmethod
+    def push(new_dir):
+        """ Push curdir to stack and change to new_dir. """
+        curdir = os.path.realpath(os.curdir)
+        PDir.dirs.append(curdir)
+        os.chdir(new_dir)
+        print("Changing to: " + new_dir)
+    @staticmethod
+    def pop():
+        """ Pop the dirstack and return to it. """
+        os.chdir(PDir.dirs.pop())
+
+# Functions
+
+def gen_report(progress):
+    """ Report hook generator. """
+    def report_down(block_count, bytes_per_block, total_size):
+        """ Simple report hook. """
+        if block_count == 0:
+            print("Download Started")
+        elif total_size < 0:
+            print("Read %d blocks" % block_count)
+        else:
+            total_down = block_count * bytes_per_block
+            percent = (total_down * 100.0) / total_size
+            if total_down >= total_size or progress.check_percent(percent):
+                progress.draw()
+    return report_down
+
+def get_code(command, target):
+    """ Wrapper function to clone repos.
+    Protects against overwriting if target exists.
+    command: The command that would run in bash.
+    target: Where to clone to.
+    """
+    cmd = command.split()
+    cmd.append(target)
+    if not os.path.exists(target):
+        subprocess.call(cmd)
+
+def num_jobs():
+    """ Use BASH one liner to determine number of threads available. """
+    jobs = subprocess.check_output('cat /proc/cpuinfo | grep "processor"\
+            | wc -l', shell=True)
+    return int(jobs)
+
+def build_sdl(libdir):
+    """ Build ack from source, move to target dir. """
+    srcdir = 'sdl'
+    srcdir2 = srcdir + '2'
+
+    try:
+        # Fetch code & copy for 2
+        get_code('hg clone -u SDL-1.2 http://hg.libsdl.org/SDL', srcdir)
+        cmd = 'cp -r {} {}'.format(srcdir, srcdir + '2').split()
+        subprocess.call(cmd)
+
+        # Build sdl1
+        PDir.push(srcdir)
+        subprocess.call('./autogen.sh')
+        cmd = './configure --prefix={}'.format(libdir).split()
+        subprocess.call(cmd)
+        cmd = 'make -j{} install'.format(num_jobs()).split()
+        subprocess.call(cmd)
+        PDir.pop()
+
+        # Build sdl2
+        PDir.push(srcdir2)
+        cmd = 'hg update default'.split()
+        subprocess.call(cmd)
+        cmd = './configure --prefix={}'.format(libdir).split()
+        subprocess.call(cmd)
+        cmd = 'make -j{} install'.format(num_jobs()).split()
+        subprocess.call(cmd)
+        PDir.pop()
+    finally:
+        shutil.rmtree(srcdir)
+        shutil.rmtree(srcdir2)
+
+def build_gtest(libdir):
+    """ Build gtest from source and put in libs. """
+    archive = 'gtest.zip'
+    url = 'https://googletest.googlecode.com/files/gtest-1.7.0.zip'
+
+    try:
+        # Fetch program
+        print("Downloading gtest source.")
+        prog = Progress.default_prog()
+        zfile = urllib.URLopener()
+        zfile.retrieve(url, archive, gen_report(prog))
+        zipfile.ZipFile(archive).extractall()
+        srcdir = glob.glob('gtest-*')[0]
+
+        # Build & clean
+        PDir.push(srcdir)
+        cmd = 'chmod u+x configure ./scripts/*'.split()
+        subprocess.call(cmd)
+        cmd = './configure --prefix={}'.format(libdir).split()
+        subprocess.call(cmd)
+        subprocess.call('make')
+
+        # Copy out file structure
+        os.mkdir(libdir + os.sep + 'lib')
+        shutil.copytree('include', libdir + os.sep + 'include')
+        for fil in glob.glob('lib' + os.sep + '.libs' + os.sep + '*.a'):
+            shutil.copy(fil, libdir + os.sep + 'lib')
+    finally:
+        PDir.pop()
+        os.remove(archive)
+        shutil.rmtree(srcdir)
+
+def build_cunit(libdir):
+    """ Build classic cunit, good test lib for c code. """
+    srcdir = 'cunit'
+    get_code('svn checkout svn://svn.code.sf.net/p/cunit/code/trunk', srcdir)
+
+    # Build & clean
+    PDir.push(srcdir)
+    # Shell true is due to some bug via normal way
+    subprocess.call('./bootstrap {}'.format(libdir), shell=True)
+    cmd = 'make -j{} install'.format(num_jobs()).split()
+    subprocess.call(cmd)
+    PDir.pop()
+
+    shutil.rmtree(srcdir)
+
+def main():
+    """ Main function. """
+    mesg = """This script sets up the libs for this project."""
+    parser = argparse.ArgumentParser(description=mesg,
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('target', nargs='?', default='libs',
+            help='dir to put libs in')
+
+    args = parser.parse_args()  # Default parses argv[1:]
+    libdir = os.path.realpath(os.curdir + os.sep + args.target)
+
+    if os.path.exists(libdir):
+        raise OSError('Directory already exists. {}'.format(libdir))
+
+    os.makedirs(libdir)
+
+    build_gtest(libdir)
+    build_sdl(libdir)
+    build_cunit(libdir)
+
+if __name__ == '__main__':
+    main()
