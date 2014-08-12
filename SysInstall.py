8df19ea8706eed37f9163d9ccca32f1a583bab2a
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-08-12 19:56:43 -0400

    Add sharutils for uuencode & co.

--- SysInstall.py
+++ SysInstall.py
@@ -39,7 +39,7 @@ PROGRAMS = """ \
     chromium-browser firefox rekonq \
     abiword baobab dvdrip easytag chm2pdf dia catdoc \
     gimp gimp-plugin-registry \
-    fuse gparted quicksynergy tree dfc \
+    fuse gparted quicksynergy tree dfc sharutils sharutils-doc \
     libnotify-bin libnotify-dev \
     redshift geoclue geoclue-hostip numlockx \
     samba samba-doc samba-dev samba-dbg wireshark libwireshark-dev wondershaper \
