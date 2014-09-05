fab433c9c6264ced69f8e509cc9881403df2e201
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-09-05 08:52:58 -0400

    Enable error num on prompt.

--- dot_files/.git-prompt-colors.sh
+++ dot_files/.git-prompt-colors.sh
@@ -23,10 +23,10 @@ define_git_prompt_colors() {
   # will be replaced with the exit code of the last command
   # e.g.
   # GIT_PROMPT_COMMAND_OK="${Green}✔-_LAST_COMMAND_STATE_ "    # indicator if the last command returned with an exit code of 0
-  # GIT_PROMPT_COMMAND_FAIL="${Red}✘-_LAST_COMMAND_STATE_ "   # indicator if the last command returned with an exit code of other than 0
+   GIT_PROMPT_COMMAND_FAIL="${Red}✘${Yellow}-_LAST_COMMAND_STATE_ "   # indicator if the last command returned with an exit code of other than 0
 
   GIT_PROMPT_COMMAND_OK="${Green}✔ "    # indicator if the last command returned with an exit code of 0
-  GIT_PROMPT_COMMAND_FAIL="${Red}✘ "   # indicator if the last command returned with an exit code of other than 0
+  #GIT_PROMPT_COMMAND_FAIL="${Red}✘ "   # indicator if the last command returned with an exit code of other than 0
 
   GIT_PROMPT_START_USER="${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_START_ROOT="${Yellow}${PathShort}${ResetColor}"
