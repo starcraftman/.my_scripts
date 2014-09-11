37abf522975eb2708c0e731f0e47f60ddbab5c9a
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-09-11 14:13:49 -0400

    Naming.

--- archive/vim_colors.sh
+++ archive/vim_colors.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 # Update the color schemes in .vim/colors
-
 DST=~/.vim/colors
 URL=http://github.com/
-COLORS=( 'chriskempson/vim-tomorrow-theme' 'nanotech/jellybeans.vim' 'Lokaltog/vim-distinguished' \
+THEMES=( 'chriskempson/vim-tomorrow-theme' 'nanotech/jellybeans.vim' 'Lokaltog/vim-distinguished' \
 'morhetz/gruvbox' 'tomasr/molokai' 'tpope/vim-vividchalk' 'vim-scripts/desert256.vim' )
 
-update_colors()
+update_themes()
 {
-    for color ; do
-        local gUrl="$URL$color"
-        local dir="${color##*/}"
+    for theme ; do
+        local gUrl="$URL$theme"
+        local dir="${theme##*/}"
         git clone "$gUrl"
         cp -f ./$dir/colors/*.vim $DST
         rm -rf "$dir"
     done
 }
 
-update_colors ${COLORS[@]}
+update_themes ${THEMES[@]}
