b2ee02334b9d4cefa7fa0c40734a7967b4f3ef2d
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-10-16 19:41:33 -0400

    Proper interface for bootstraper, name bad.

--- archive/bootstrap_py_and_zsh.sh
+++ archive/bootstrap_py_and_zsh.sh
@@ -30,6 +30,35 @@ build_zsh()
     rm -rf zsh
 }
 
-echo "Simple bootstrapper to get python & zsh on systems that might\
-not have packages for it."
-echo "Change DIR to point to the target to install to."
+usage() {
+    echo "$(basename $0) arg1 arg2 ...
+
+    dir=newPath >>> set DIR to install to, be careful.
+    py          >>> build python 2 and install to DIR.
+    zsh         >>> build latest zsh, install to DIR.
+    "
+}
+
+if [ "$#" -lt 1 -o "$1" == '--help' -o "$1" == '-h' ]; then
+    usage
+    exit
+fi
+
+for arg; do
+    case "$arg" in
+        py)
+            #build_py
+            echo $DIR
+            ;;
+        zsh)
+            #build_zsh
+            ;;
+        dir*)
+            DIR=${arg##*=}
+            ;;
+        *)
+            usage
+            exit
+            ;;
+    esac
+done
