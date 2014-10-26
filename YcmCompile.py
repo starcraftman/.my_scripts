fca04af934ce35b18d41905675ce00db42c99c06
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-10-26 14:17:32 -0400

    Simplify ycm builder, still can't quite fit in BuildSrc.

--- YcmCompile.py
+++ YcmCompile.py
@@ -6,10 +6,11 @@
 """ This is a simple script to recompile ycm everytime it gets updated. """
 
 # Imports
+from BuildSrc import get_archive
+import multiprocessing
 import os
 import shutil
 import subprocess
-import SysInstall
 
 # Data
 CLANG_URL = 'http://llvm.org/releases/3.5.0/\
@@ -36,25 +37,20 @@ def build_ycm():
     os.chdir(B_DIR)
 
     cmds = [
-        [
-            'cmake', '-GUnix Makefiles',
-            '-DPATH_TO_LLVM_ROOT=../{}'.format(CLANG_DIR), '.',
-            '~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp',
-        ],
-        [
-            'make', '-j', unicode(SysInstall.NUM_JOBS), 'ycm_support_libs',
-        ],
+        'cmake -DPATH_TO_LLVM_ROOT=../%s . \
+~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp' % CLANG_DIR,
+        'make -j%d ycm_support_libs' % multiprocessing.cpu_count(),
     ]
 
     for cmd in cmds:
-        subprocess.call(cmd)
+        subprocess.call(cmd.split())
 
 def main():
     """ Main function. """
     origdir = os.path.abspath(os.curdir)
 
     try:
-        SysInstall.get_archive(CLANG_URL, CLANG_DIR)
+        get_archive(CLANG_URL, CLANG_DIR)
         build_ycm()
     finally:
         os.chdir(origdir)
