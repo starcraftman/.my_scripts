e885d859ae978dc0d09b4d2156c188c2ac6d82b3
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2018-05-11 10:52:22 -0400

    Rename for templates & higher compress mp3.

--- bin/flac2mp3.sh
+++ bin/flac2mp3.sh
@@ -24,10 +24,10 @@ find . -name "*.flac" > input.txt
 
 # Encode mp3 to 160kbs, alternatives: https://trac.ffmpeg.org/wiki/Encode/MP3
 if valid_name parallel; then
-    parallel -a input.txt avconv -i "{}" -qscale:a 4 -map_metadata 0:g:0 "{.}.mp3"
+    parallel -a input.txt avconv -i "{}" -qscale:a 5 -map_metadata 0:g:0 "{.}.mp3"
 else
     while read file; do
-        avconv -i "$file" -qscale:a 4 -map_metadata 0:g:0 "${file%flac}mp3"
+        avconv -i "$file" -qscale:a 5 -map_metadata 0:g:0 "${file%flac}mp3"
     done
 fi
 
