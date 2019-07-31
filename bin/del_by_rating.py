e41d7ff73955a04fd59ce1cf01d58e30e0ea6673
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2019-07-30 22:34:00 -0400

    Instead of del, mv matching files.

--- bin/del_by_rating.py
+++ bin/del_by_rating.py
@@ -6,14 +6,37 @@ Affects only FLAC and MP3 files.
 """
 import argparse
 import functools
+import hashlib
 import multiprocessing as multi
 import os
+import shutil
 import subprocess as sub
 import sys
 import pathlib
 
 
-def flac_rating(cutoff, fname):
+def same_files(fname, fname2):
+    """
+    Two files are the same if they have the same hash.
+
+    :param fname String: The first filename.
+    :param fname2 String: The second filename.
+    """
+    try:
+        hash1 = hashlib.sha512()
+        with open(fname, 'rb') as fin:
+            hash1.update(fin.read())
+
+        hash2 = hashlib.sha512()
+        with open(fname2, 'rb') as fin:
+            hash2.update(fin.read())
+
+        return hash1.hexdigest() == hash2.hexdigest()
+    except IOError:
+        return False
+
+
+def flac_rating(cutoff, destination, fname):
     """
     FLAC rating of a file. 0 if no rating found.
 
@@ -27,11 +50,17 @@ def flac_rating(cutoff, fname):
             rating = float(line.split('=')[1])
             break
 
-    if rating < cutoff:
-        os.remove(fname)
+    if rating >= cutoff:
+        new_file = os.path.join(destination, fname)
+        try:
+            os.makedirs(os.path.dirname(new_file))
+        except OSError:
+            pass
+        if not os.path.exists(new_file) and not same_files(fname, new_file):
+            shutil.copyfile(fname, new_file)
 
 
-def mp3_rating(cutoff, fname):
+def mp3_rating(cutoff, destination, fname):
     """
     MP3 rating of a file. 0 if no rating found.
 
@@ -44,8 +73,14 @@ def mp3_rating(cutoff, fname):
             rating = float(line.split('=')[2])
             break
 
-    if rating < cutoff:
-        os.remove(fname)
+    if rating >= cutoff:
+        new_file = os.path.join(destination, fname)
+        try:
+            os.makedirs(os.path.dirname(new_file))
+        except OSError:
+            pass
+        if not os.path.exists(new_file) and not same_files(fname, new_file):
+            shutil.copyfile(fname, new_file)
 
 
 def make_parser():
@@ -54,6 +89,7 @@ def make_parser():
     """
     parser = argparse.ArgumentParser(description="Delete songs below cutoff rating")
     parser.add_argument('folder', help="The folder that you want to scrape.")
+    parser.add_argument('destination', help="The folder that you want to copy to on match.")
     parser.add_argument('--cutoff', '-c', type=float, default=1.0,
                         help="The cutoff to use. Default 1.0")
 
@@ -70,12 +106,14 @@ def main():
     else:
         pat = pathlib.Path(".")
 
-    mp3s = pat.glob("**/*.mp3")
-    flacs = pat.glob("**/*.flac")
+    mp3s = list(pat.glob("**/*.mp3"))
+    flacs = list(pat.glob("**/*.flac"))
 
     with multi.Pool(8) as pool:
-        pool.map(functools.partial(flac_rating, args.cutoff), flacs)
-        pool.map(functools.partial(mp3_rating, args.cutoff), mp3s)
+        if flacs:
+            pool.map(functools.partial(flac_rating, args.cutoff, args.destination), flacs)
+        if mp3s:
+            pool.map(functools.partial(mp3_rating, args.cutoff), args.destination, mp3s)
 
 
 if __name__ == "__main__":
